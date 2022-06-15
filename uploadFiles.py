462feefb306a4cc89a30aca567848064f7cd8304
Asmi Rao <seldonbeeblebrox42@gmail.com>
2022-06-16 01:10:45 +0530

    Project 101

---
+++ uploadFiles.py
@@ -0,0 +1,33 @@
+import dropbox
+import os
+
+class TransferData ():
+
+    def __init__( self, access_token ):
+        self.access_token: access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        f = open(file_from, 'rb')
+        dbx.files_upload(f.read(), file_to)
+
+def main():
+
+    access_token = 'sl.BJkLdej83a1O15BCIeWHShGuvNHdx-rgVumyjwm62xq8hJbkHaGe-GotJgXqSaOXFeNGFCP3tidJx3tfsPyA8poBVh_mmtiAMfIa4Kv5R_AKvsTomq0RGCI2ZejaKqofkeBvtKCP8H4'
+
+    file_from = input("Enter the path of the file to be uploaded ")
+    file_to = input("Enter the full path to be uploaded to dropbox ")
+
+    transfer_data = TransferData(access_token)
+
+    transfer_data.upload_file(file_from, file_to)
+    print("File has been uploaded")
+
+    for (root, dirs, files) in os.walk('/Users/drpoojayadav/Downloads/untitled folder/luftballoons/nadia/Alan', topdown = True):
+        print(root)
+        print(dirs)
+        print(files)
+
+
+main()
